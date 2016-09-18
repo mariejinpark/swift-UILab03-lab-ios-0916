52e3f1c3566c2f31fa43c0030a0be24edf249d03
mariejinpark <mariejinpark@gmail.com>
2016-09-17 23:54:33 -0400

    Done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+   // TODO: Create IB outlets
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +27,33 @@ class ViewController: UIViewController {
     // TODO: IB actions and code to update UI
 
   
+    @IBAction func touchClubButton(_ sender: AnyObject) {
+        mainLabel.text = "4"
+        topLeft.text = "♣️"
+        bottomRight.text = "♣️"
+    }
+    
+    @IBAction func touchSpadeButton(_ sender: AnyObject) {
+        mainLabel.text = "3"
+        topLeft.text = "♠️"
+        bottomRight.text = "♠️"
+    }
+    
+    @IBAction func touchDiamondButton(_ sender: AnyObject) {
+        mainLabel.text = "8"
+        topLeft.text = "♦️"
+        bottomRight.text = "♦️"
+    }
+    
+    @IBAction func touchHeartButton(_ sender: AnyObject) {
+        mainLabel.text = "10"
+        topLeft.text = "♥️"
+        bottomRight.text = "♥️"
+    }
+    
 
+    
+    
+    
+    
 }
